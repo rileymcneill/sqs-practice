4acd0bc3edd9c0c90c60b38eb026c1659216155c
Riley McNeill <mqt3uz@virginia.edu>
2024-04-18 20:11:28 +0000

    made a lot of updates and added files

--- get-attributes.py
+++ get-attributes.py
@@ -5,11 +5,11 @@ import json
 sqs = boto3.client('sqs')
 QueueUrl='https://sqs.us-east-1.amazonaws.com/654654587885/myqueue'
 
-response = sqs.receive_message(
-    QueueUrl=QueueUrl,
+response = sqs.get_queue_attributes(
+    QueueUrl,
     AttributeNames=[
         'All'
     ]
 )
 
-print(response['Messages'][0]['Body'])
+print(response['Attributes']['ApproximateNumberOfMessages'])
--- send-message.py
+++ send-message.py
@@ -2,3 +2,24 @@ import boto3
 import requests
 import json
 
+sqs = boto3.client('sqs')
+
+id = requests.get('https://ids.pods.uvarc.io')
+
+response = sqs.send_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/654654587885/myqueue',
+    MessageBody=id.text,
+    MessageAttributes={
+        'Project': {
+            'StringValue': 'Project X',
+            'DataType': 'String'
+        },
+         'Flavor': {
+            'StringValue': 'Vanilla',
+            'DataType': 'String'
+        }
+    }
+)
+
+print(response)
+
